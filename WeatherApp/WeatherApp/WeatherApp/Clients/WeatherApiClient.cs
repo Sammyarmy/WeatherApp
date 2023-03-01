80ba3e8179d868bbc2823c89d293237bb53e4a78
Sam Armstrong <Sam.Armstrong@sorted.com>
2023-03-01 02:35:56 +0000

    add http client for WeatherApi, add program main and controller

---
+++ WeatherApp/WeatherApp/WeatherApp/Clients/WeatherApiClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using WeatherApp;
+
+namespace WeatherApp.Clients;
+
+public interface IWeatherClient
+{
+    Task<WeatherForecast> GetWeatherLiveFromApi(string location);
+}
+
+public class WeatherApiClient : IWeatherClient
+{
+    private readonly HttpClient _httpClient;
+
+    private const string WeatherApiKey = "0b06ab74843f4a11aa4151554222512";
+
+    public WeatherApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<WeatherForecast?> GetWeatherLiveFromApi(string location)
+    {
+        return await _httpClient.GetFromJsonAsync<WeatherForecast>($"current.json?key={WeatherApiKey}&q={location}&aqi=no");
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Clients;
+
+namespace WeatherApp.Controllers
+{
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherClient _weatherApi;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherClient weatherApi)
+        {
+            _logger = logger;
+            _weatherApi = weatherApi;
+        }
+
+        [EnableCors("AllowSpecificOrigin")]
+        [HttpGet("/weatherforecast/{location}")]
+        public async Task<WeatherForecast> GetWeatherLive(string location)
+        {
+            return await _weatherApi.GetWeatherLiveFromApi(location);
+        }
+    }
+}
